33e9a075728edeca01c2f80b07e070a748e67cba
Senior <abhishek234543@gmail.com>
2023-01-29 20:14:02 +0530

    adding routes

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,12 @@
-const router = require('express').Router()
-const userController = require('../controllers/user')
-const gameController = require('../controllers/game')
+const router = require("express").Router();
+const userController = require("../controllers/user");
+const gameController = require("../controllers/game");
 
-router.post('/login', userController.login)
-router.post('/register', userController.register)
-router.post('/home/start', gameController.start)
-router.post('/home/invite', gameController.invite)
-router.post('/home/startGame', gameController.startgame)
+router.post("/login", userController.login);
+router.post("/register", userController.register);
+router.post("/start", gameController.start);
+router.post("/invite", gameController.invite);
+router.post("/invitation", gameController.invitation);
+router.post("/startGame", gameController.startgame);
 
-module.exports = router
+module.exports = router;
